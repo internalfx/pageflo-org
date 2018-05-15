b658770913ed6fe3ebc1b2d88170aa969915d25c
Bryan Morris <bryan@internalfx.com>
2018-05-15 14:40:48 -0700

    update service

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-sudo systemctl stop www-pageflo-org.service
+sudo systemctl stop pageflo-org.service
 git reset --hard HEAD
 git pull
 yarn
 gulp
-sudo systemctl start www-pageflo-org.service
+sudo systemctl start pageflo-org.service
